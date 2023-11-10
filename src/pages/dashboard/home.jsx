52fba6694fb0d34d03fb688642ba8c9a7f42ea1c
QuangPhat <phatdlqps22206@gmail.com>
2023-11-10 10:24:24 +0700

    1

--- src/pages/dashboard/home.jsx
+++ src/pages/dashboard/home.jsx
@@ -34,7 +34,6 @@ export function Home() {
   return (
     <div className="mt-12">
       <div className="mb-12 grid gap-y-10 gap-x-6 md:grid-cols-2 xl:grid-cols-4">
-        <h2>Xin chao</h2>
         {statisticsCardsData.map(({ icon, title, footer, ...rest }) => (
           <StatisticsCard
             key={title}
